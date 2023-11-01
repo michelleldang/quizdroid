72fbece0f64d6b6e472602d4872891b59b638661
mldang <mldang@uw.edu>
2023-10-31 23:08:25 -0700

    overview page

--- app/src/main/java/edu/uw/ischool/mldang/quizdroid/TopicOverviewActivity.kt
+++ app/src/main/java/edu/uw/ischool/mldang/quizdroid/TopicOverviewActivity.kt
@@ -7,6 +7,7 @@ import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class TopicOverviewActivity: AppCompatActivity() {
+    private var questionList: ArrayList<Question>? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_topic_overview)
@@ -14,16 +15,31 @@ class TopicOverviewActivity: AppCompatActivity() {
         val topic = intent.getStringExtra("topic")
         var description = findViewById<TextView>(R.id.topicDescription)
         val topicString = when (topic) {
-            "Math" -> R.string.math
-            "Physics" -> R.string.physics
-            else -> R.string.marvel
+            "Math" -> {
+                getMathQuestions()
+                R.string.math}
+            "Physics" -> {
+                getPhysicsQuestions()
+                R.string.physics}
+            else -> {
+                getMarvelQuestions()
+                R.string.marvel}
         }
-        description.text = getString(topicString)
+        description.text = getString(topicString) + "\n\nThere are ${questionList?.size} questions in this topic"
 
         val beginButton= findViewById<Button>(R.id.begin)
         beginButton.setOnClickListener {
             val intent = Intent(this, QuestionActivity::class.java).putExtra("topic", topic)
             startActivity(intent)
         }
+    }
+    private fun getMathQuestions() {
+        questionList = Questions.getMathQuestions()
+    }
+    private fun getPhysicsQuestions() {
+        questionList = Questions.getPhysicsQuestions()
+    }
+    private fun getMarvelQuestions() {
+        questionList = Questions.getMarvelQuestions()
     }
 }
